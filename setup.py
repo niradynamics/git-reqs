04da33c53d2ef9cf16db610b4d6aab0bbb6b1c73
Dennis Millard <dennis.millard@niradynamics.se>
2020-09-30 10:56:16 +0200

    Code updated to enable download using pip3

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'git-reqs',
+  packages = ['git-reqs'],
+  version = '0.1',
+  description = 'Simple requirement management system for developers based yaml, networkx and git',
+  author = 'Jonas Josefsson',
+  author_email = 'Jonas.josefsson@niradynamics.se',
+  url = 'https://github.com/niradynamics/git-reqs',
+  download_url = 'https://github.com/niradynamics/git-reqs/archive/0.1.tar.gz',
+  keywords = ['Requirements', 'gitreqs'],
+  install_requires=[
+          'junitparser',
+          'matplotlib',
+          'networkx',
+          'numpy',
+          'pydot',
+          'PyYAML',
+          'xlrd',
+          'xlwt',
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',
+    'Intended Audience :: Developers',
+    'Topic :: Software Development',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
