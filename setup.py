41fa8bb913d6a4ed0a517fcb909d2d6fa4480f1e
Jonas Josefsson <jonas.josefsson@niradynamics.se>
2020-10-01 08:31:01 +0200

    Updated version to 0.1.4

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 setup(
   name = 'git_reqs',
   packages = ['git_reqs'],
-  version = '0.1.3',
+  version = '0.1.4',
   description = 'Simple requirement management system for developers based yaml, networkx and git',
   author = 'Jonas Josefsson',
   author_email = 'Jonas.josefsson@niradynamics.se',
